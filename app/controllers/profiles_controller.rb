1c3c536fc8cf55e9700e00614e8901d1e6a735bb
user <zhensongjianaizi@gmail.com>
2020-10-05 17:42:02 +0900

    profilel更新

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,31 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+      @profile = current_user.profile
+  end
+
+  def edit
+      @profile = current_user.prepare_profile
+  end
+  
+  def update
+      @profile = current_user.prepare_profile
+      @profile.assign_attributes(profile_params)
+      if @profile.save
+          redirect_to profile_path, notice: 'Profile Update!'
+      else
+          flash.now[:error] = "I couldn't update."
+          render :edit
+      end
+  end
+
+  private
+  def profile_params
+      params.require(:profile).permit(
+          :nickname,
+          :introduction,
+          :avatar
+      )
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
+
+  def display_name
+    profile&.nickname || self.email
+  end
 end
