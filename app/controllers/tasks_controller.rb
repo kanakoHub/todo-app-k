8ec7b05c5b76b220174b7e45a6abbbcd083caac4
user <zhensongjianaizi@gmail.com>
2020-10-07 14:38:24 +0900

    taskのedit,updateアクション

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,37 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_task, only: [:edit, :update]
 
   def show
     board = Board.find(params[:board_id])
     @task = board.tasks.find(params[:id])
   end
 
+  def edit
+
+  end
+
+  def update
+      if @task.update(task_params)
+          redirect_to board_task_path(@task.board_id, @task.id), notice: 'Task Update!'
+      else
+          flash.now[:error] = "I couldn't update."
+          render :edit
+      end
+  end
+
+  # def destroy
+  #     board = current_user.boards.find(params[:id])
+  #     board.destroy!
+  #     redirect_to root_path, notice: "I deleted it."
+  # end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description, :closing_day, :eyecatch)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 end
